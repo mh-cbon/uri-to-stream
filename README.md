af205d9ed6694ad2e41a1edc29b9824fb2bb05c6
mh-cbon <mh-cbon@users.noreply.github.com>
2016-05-10 22:28:23 +0200

    support object uri

--- README.md
+++ README.md
@@ -15,11 +15,44 @@ var uriToStream = require('uri-to-stream');
 
 uriToStream.read(__dirname + '/index.js').pipe(process.stdout);
 uriToStream.read('http://google.com').pipe(process.stdout);
-// uriToStream.read('tcp://something.to.read:1337');
+uriToStream.read('tcp://something.to.read:1337');
 
 
 uriToStream.write(__dirname + '/test/write').end('tomate');
 uriToStream.write('http://google.com').end('yahoo');
-// uriToStream.write('tcp://something.to.write:1337');
+uriToStream.write('tcp://something.to.write:1337');
 
 ```
+
+For convenience it also supports Object addresses
+
+```js
+var uriToStream = require('uri-to-stream');
+
+uriToStream.read({
+  protocol: 'http:',
+  port: '8000',
+  hostname: '127.0.0.1'
+}).pipe(process.stdout);
+
+uriToStream.read({
+  port: '8000',
+  host: '127.0.0.1'
+});
+
+uriToStream.write({
+  protocol: 'http:',
+  port: '8000',
+  hostname: '127.0.0.1'
+}).end('tomate');
+
+uriToStream.write({
+  port: '8000',
+  host: '127.0.0.1'
+}).end('yahoo');
+
+```
+
+# todo
+
+- support ssl
--- index.js
+++ index.js
@@ -6,9 +6,13 @@ var ip    = require('ip');
 
 var uriToReadStream = function (uri, opts) {
   var stream;
-  if (uri.match(/^http/)) {
+  if (typeof(uri)==='string' && uri.match(/^http/) || uri.protocol && uri.protocol.match(/http/)) {
     stream = miss.through();
-    var options = Object.assign({}, opts, url.parse(uri));
+
+    var options;
+    if (typeof(uri)==='string') options = Object.assign({}, opts, url.parse(uri));
+    else options = Object.assign({}, opts, uri);
+
     var data = options.data || '';
     delete options.data;
     delete options.host;
@@ -33,43 +37,75 @@ var uriToReadStream = function (uri, opts) {
       stream.emit('error', e);
     }).end(data);
 
-  } else if (uri.match(/^tcp/)) {
-    uri = url.parse(uri);
-    var host = uri.hostname || uri.host;
-    var options = Object.assign({}, opts, {
-      port: uri.port,
-      host: host,
-      family: (ip.isV4Format(host) && '4') || (ip.isV6Format(host) && '6') || null
-    })
+  } else if (typeof(uri)==='string' && uri.match(/^tcp/) || uri.port && uri.host) {
+
+    var options;
+    if (typeof(uri)==='string') {
+      uri = url.parse(uri);
+      var host = uri.hostname || uri.host;
+      var options = Object.assign({}, opts, {
+        port: uri.port,
+        host: host,
+        family: (ip.isV4Format(host) && '4') || (ip.isV6Format(host) && '6') || null
+      })
+    } else {
+      options = Object.assign({}, opts, url.parse(uri));
+    }
+
     stream = net.createConnection(options);
 
-  } else {
+  } else if(typeof(uri)==='string') {
     stream = require('fs').createReadStream(uri, opts);
+
+  } else {
+    process.nextTick(function () {
+      stream.emit('error', new Error('unhandled uri : ' + JSON.stringify(uri)))
+    })
   }
   return stream;
 }
 var uriToWriteStream = function (uri, opts) {
   var stream;
-  if (uri.match(/^http/)) {
+  if (typeof(uri)==='string' && uri.match(/^http/) || uri.protocol && uri.protocol.match(/http/)) {
+
     if(typeof(opts)==='string') opts = {method: opts};
-    var options = Object.assign({}, opts, url.parse(uri));
-    delete options.host;
-    if(!options.method) options.method = 'POST';
+    if(!opts) opts = {}
+    if(!opts.method) opts.method = 'POST';
+
+    var options;
+    if (typeof(uri)==='string') options = Object.assign({}, opts, url.parse(uri));
+    else options = Object.assign({}, opts, uri);
+
+    if(options.hostname) delete options.host;
+
     // not that GET won t let you write.
     stream = http.request(options)
 
-  } else if (uri.match(/^tcp/)) {
+  } else if (typeof(uri)==='string' && uri.match(/^tcp/) || uri.port && uri.host) {
     stream = miss.through();
-    uri = url.parse(uri);
-    var host = uri.hostname || uri.host;
-    var options = Object.assign({}, opts, {
-      port: uri.port,
-      host: host,
-      family: (ip.isV4Format(host) && '4') || (ip.isV6Format(host) && '6') || null
-    })
+
+    var options;
+    if (typeof(uri)==='string') {
+      uri = url.parse(uri);
+      var host = uri.hostname || uri.host;
+      var options = Object.assign({}, opts, {
+        port: uri.port,
+        host: host,
+        family: (ip.isV4Format(host) && '4') || (ip.isV6Format(host) && '6') || null
+      })
+    } else {
+      var options = Object.assign({}, uri)
+    }
+
     stream = net.createConnection(options)
-  } else {
+
+  } else if(typeof(uri)==='string') {
     stream = require('fs').createWriteStream(uri, opts);
+
+  } else {
+    process.nextTick(function () {
+      stream.emit('error', new Error('unhandled uri : ' + JSON.stringify(uri)))
+    })
   }
   return stream;
 }
